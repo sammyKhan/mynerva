442e5a9f92507eec172a70d8de5a71d699cf7586
Sam Khan <samk1200@gmail.com>
2013-09-17 21:26:15 -0400

    Added basic UI

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
+$('#btn').click(function() {
+  var crns = $('#crns').val().replace(/,/g, '-');
+  var url = window.location.href + crns + '.ics';
+  $('#answer').html('<p>Copy the following url into Google Calendar:</p>' + url);
+});
 
